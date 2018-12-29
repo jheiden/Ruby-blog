a5362503dcb809501eb1cb8e74ce5e5c796b9a97
Johan Heidenberg <jheidenberg@outlook.com>
2018-12-29 21:19:05 +0100

    body not displaying as it should

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+    include ArticlesHelper
+
     def index
         @articles = Article.all
     end
@@ -8,7 +10,18 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
 
+    def new
+        @article = Article.new
+    end
 
+# body not displayed as it should.
+
+    def create
+    @article = Article.new(article_params)
+    @article.save
+    
+    redirect_to article_path(@article)
+    end
 
 
 
