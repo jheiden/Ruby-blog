ee378b7d58b511874124f47c6836cccec08eafd5
Johan Heidenberg <jheidenberg@outlook.com>
2018-12-30 19:57:44 +0100

    finished comments feature

--- blogger/config/routes.rb
+++ blogger/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'articles#index' # changes root ("home") to article index list
-  resources :articles
+  resources :articles do
+    resources :comments
+end
+
 end
