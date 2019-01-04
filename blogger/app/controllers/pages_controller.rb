ccf7c993bd97cce497f4551ddaf29a35ede0fba2
Johan Heidenberg <jheidenberg@outlook.com>
2019-01-04 14:50:39 +0100

    deleted inner folder

--- blogger/app/controllers/pages_controller.rb
+++
@@ -1,7 +0,0 @@
-class PagesController < ApplicationController
-    def about
-        @title = "About this page"
-        @content = "This page was created by Johan Heidenberg as a part of the odins project curriculum"
-    end
-
-end
